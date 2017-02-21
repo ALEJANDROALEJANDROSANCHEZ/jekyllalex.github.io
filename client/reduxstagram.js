9c08c8b90b850fd3dfd34a97152dee43c3c1d7f3
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-02-20 22:19:15 -0500

    Implement basic routes

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {render} from "react-dom"
 import css from "./styles/style.styl"
-import Main from "./components/main"
+import router from "./components/router"
 
-render(<Main />, document.getElementById("root"))
+render(router, document.getElementById("root"))
+
